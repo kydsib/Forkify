0f1f373e3ba37894c886e6f5e567c30421d7ef82
kydsib <simonas.kersulis@gmail.com>
2019-08-13 11:10:38 +0300

    Set up the food2fork API with axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module aint one! Variable x is ${x}`);
+async function getResults(query) {
+    const key = '298560efeb6876ee6445c8625f6b5f8d';
+    try {
+        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+
+}
+
+getResults('pizza');
